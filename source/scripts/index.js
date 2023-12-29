ecdec7f5ee3abbb16e0dcaaad0f4c771c9b03207
CatInBoots <oom070392@gmail.com>
2023-12-29 01:26:21 +0100

    Index stylization ongoing

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+document.addEventListener('DOMContentLoaded', function () {
+  var toggleButton = document.querySelector('.main-header__toggle');
+  var nav = document.querySelector('.main-nav');
+
+  toggleButton.addEventListener('click', function () {
+    if (nav.style.display === 'none' || nav.style.display === '') {
+      nav.style.display = 'block';
+      toggleButton.classList.add('main-header__toggle--open');
+    } else {
+      nav.style.display = 'none';
+      toggleButton.classList.remove('main-header__toggle--open');
+    }
+  });
+});
