0abe2ef6732e3eba96f69176f8daeae3605521f2
CatInBoots <oom070392@gmail.com>
2024-02-03 13:16:57 +0100

    fix: сopying static assets to build

--- gulpfile.js
+++ gulpfile.js
@@ -168,7 +168,7 @@ export function startServer () {
   watch(`${PATH_TO_SOURCE}styles/**/*.scss`, series(processStyles));
   watch(`${PATH_TO_SOURCE}scripts/**/*.js`, series(processScripts));
   watch(`${PATH_TO_SOURCE}images/icons/**/*.svg`, series(createStack, reloadServer));
-  watch(PATHS_TO_STATIC, series(copyAssets, reloadServer));
+  watch(PATHS_TO_STATIC, series(reloadServer));
 }
 
 function reloadServer (done) {
@@ -193,7 +193,7 @@ export function buildProd (done) {
       processStyles,
       processScripts,
       createStack,
-      // copyAssets,
+      copyAssets,
     ),
   )(done);
 }
